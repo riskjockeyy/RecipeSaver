52b90768a77aed9e6908d9da919efc1e12716831
Ankita-Rawat <go2ankitarawat@gmail.com>
2022-01-05 10:57:59 +0530

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Bharat Singh on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
